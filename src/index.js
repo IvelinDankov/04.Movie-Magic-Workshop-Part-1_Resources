41efed152ad58cb50993de5ca2df6ffc2db886a4
Velin Dankov <velin80@web.de>
2024-09-24 09:34:26 +0300

    install express-handlebars, and setup server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import express from "express"
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('it\'s work')
+})
+
+app.listen(5000, () => `Server is listening on port 5000.....`)
