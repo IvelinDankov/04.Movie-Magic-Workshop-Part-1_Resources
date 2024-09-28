3d3da401c081c4860891cde806530e4298c7c018
Velin Dankov <velin80@web.de>
2024-09-28 09:53:41 +0300

    add create cast

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,9 +1,18 @@
 import { Router } from "express";
+import castService from "../services/castService.js"
 
-const router = Router()
+const router = Router();
 
-router.get('/create', (req, res) => {
-   res.render('cast/create')
+router.get("/create", (req, res) => {
+  res.render("cast/create");
+});
+
+router.post("/create", async (req, res) => {
+  const cast = req.body;
+
+ await castService.createCast(cast)
+
+  res.redirect("/home");
 });
 
 export default router;
---
+++ src/services/castService.js
@@ -0,0 +1,6 @@
+import Cast from "../models/Cast.js";
+
+const createCast = (cast) => Cast.create(cast)
+
+
+export default {createCast};
